f63e948f93e3a21836a2e8877e907b4c8757e8f7
samarth-asthana <54601188+samarth-asthana@users.noreply.github.com>
2020-09-24 00:35:48 +0530

    peep

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React ,{useState}from 'react';
+import { StyleSheet, Text, View,Button,TextButton } from 'react-native';
 
 export default function App() {
+  const[outputText,setOutputText]=useState('Hii Samarth');
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{outputText}</Text>
       <StatusBar style="auto" />
+      <Button title="Change Text" onPress={()=>setOutputText('Bye Samarth')}/>
     </View>
   );
 }
